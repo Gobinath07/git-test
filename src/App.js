28787e0f1786fda5ea98370cfc79d2e998eff377
gobinath <gobidyni07@gmail.com>
2020-05-10 04:49:30 -0500

    component 1

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import{Navbar,NavbarBrand} from 'reactstrap';
+import Menu from './components/MenuComponents';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar dark color="primary">
+        <div className="container">
+        <NavbarBrand href="/">Ristorante con fusion</NavbarBrand>
+      </div></Navbar>
+      <Menu/>
+    </div>
+  );
+}
+
+export default App;
